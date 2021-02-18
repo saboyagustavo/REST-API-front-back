6e1a1c6314bd48d928fbf14c5ffc8151c4ced29a
Gustavo Saboya <saboya.gustavo@gmail.com>
2021-02-18 17:52:01 -0300

    Amending the static list and receiving data in the main scope of the script.

--- front/main.js
+++ front/main.js
@@ -4,10 +4,11 @@ window.addEventListener('load', init);
 async function init() {
     console.log('hello, world');
     const alunos = await fetchData(path);
+    console.log(alunos); //-> Array(4) [{...}, {...}, {...}, {...}]
 }
 
 async function fetchData(path) {
     const data = await fetch(path);
     const json = await data.json();
-    console.log(json);
+    return json;
 }
